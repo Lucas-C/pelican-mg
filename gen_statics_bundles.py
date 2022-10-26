dd973dc874c6141a87c747d2dd406176bbf7a784
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2022-10-26 08:51:46 +0200

    Using <img loading="lazy" instead of lazysides JS

--- gen_statics_bundles.py
+++ gen_statics_bundles.py
@@ -8,8 +8,6 @@ JSLIBS = (
     'html5shiv-3.7.2.min',
     'jquery-1.10.2.min',
     'jquery.sticky-kit-1.1.1',
-    'lazysizes-4.0.0-rc3.min',
-    'lazysizes-4.0.0-rc3.noscript.min',
     'uikit-2.27.4.min',
     'webmention-0.2.0.min',
 )
@@ -26,22 +24,30 @@ def sed(filepath, pattern, value):
         f.write(re.sub(pattern, value, data))
         f.truncate()
 
+def sizeof_fmt(num, suffix="B"):
+    # Recipe from: https://stackoverflow.com/a/1094933/636849
+    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+        if abs(num) < 1024:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024
+    return f"{num:.1f}Yi{suffix}"
+
 
 print('Generating CSS bundle')
-short_hash = sha1(cat('static/main.css') + ''.join(CSSLIBS).encode()).hexdigest()[:7]
-css_bundle_filepath = 'static/bundle-SHORTSHA1-{}.css'.format(short_hash)
+css_short_hash = sha1(cat('static/main.css') + ''.join(CSSLIBS).encode()).hexdigest()[:7]
+css_bundle_filepath = 'static/bundle-SHORTSHA1-{}.css'.format(css_short_hash)
 files_created = not os.path.exists(css_bundle_filepath)
 with open(css_bundle_filepath, 'wb') as bundle:
     for csslib in CSSLIBS:
         bundle.write(cat('static/csslibs/{}.css'.format(csslib)))
     bundle.write(cat('static/main.css'))
-sed('templates/base.html', '-SHORTSHA1-[a-z0-9]+.css', '-SHORTSHA1-{}.css'.format(short_hash))
+print(f'* {css_bundle_filepath} :', sizeof_fmt(os.stat(css_bundle_filepath).st_size), file=sys.stderr)
 
-print('Generating JS bundle')
-short_hash = sha1(cat('static/js/social.js') + cat('static/js/filter-tags.js') + ''.join(JSLIBS).encode()).hexdigest()[:7]
+print('Generating JS bundles')
+js_short_hash = sha1(cat('static/js/social.js') + cat('static/js/filter-tags.js') + ''.join(JSLIBS).encode()).hexdigest()[:7]
 for SHARE, MG_FILTER_TAGS in itertools.product(range(2), repeat=2):
     js_bundle_filepath = 'static/bundle-SHARE-{}-MG_FILTER_TAGS-{}-SHORTSHA1-{}.js'.format(
-            SHARE, MG_FILTER_TAGS, short_hash)
+            SHARE, MG_FILTER_TAGS, js_short_hash)
     files_created |= not os.path.exists(js_bundle_filepath)
     with open(js_bundle_filepath, 'wb') as bundle:
         for jslib in JSLIBS:
@@ -50,7 +56,12 @@ for SHARE, MG_FILTER_TAGS in itertools.product(range(2), repeat=2):
             bundle.write(cat('static/js/social.js'))
         if MG_FILTER_TAGS:
             bundle.write(cat('static/js/filter-tags.js'))
-sed('templates/base.html', '-SHORTSHA1-[a-z0-9]+.js', '-SHORTSHA1-{}.js'.format(short_hash))
+    print(f'* {js_bundle_filepath} :', sizeof_fmt(os.stat(js_bundle_filepath).st_size), file=sys.stderr)
+
+print('Updating HTML with the correct SHA1 hashes in the CSS/JS bundle paths')
+sed('templates/base.html', '-SHORTSHA1-[a-z0-9]+.css', '-SHORTSHA1-{}.css'.format(css_short_hash))
+sed('templates/base.html', '-SHORTSHA1-[a-z0-9]+.js', '-SHORTSHA1-{}.js'.format(js_short_hash))
+print('* templates/base.html :', sizeof_fmt(os.stat('templates/base.html').st_size), file=sys.stderr)
 
 if files_created:
     print('New bundle files were created', file=sys.stderr)
